2a685e83e2967a0a674a5e0a25a3ad3e53b57086
Natacha Ivannikova <natacha.im@gmail.com>
2021-03-15 10:51:05 +0100

    ✨ - Change port

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 // Require the framework and instantiate it
 const fastify = require('fastify')({ logger: true })
+const port = process.env.PORT || 4000;
 
 const testimonials = require('./data/testimonials.json');
 
@@ -21,7 +22,8 @@ fastify.get('/testimonials', async (request, reply) => {
 // Run the server!
 const start = async () => {
   try {
-    await fastify.listen(7000)
+    // await fastify.listen(7000)
+    await fastify.listen(port, '0.0.0.0') // los 0.0 son para heroku para evitar el R10 error
   } catch (err) {
     fastify.log.error(err)
     process.exit(1)
